f847a37b50b3a86168649b37e47be7e9bf324d53
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-03-18 22:28:12 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+// Create a variable to store the multiplication result
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Assign a random integer to the variable 'random'
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Create a variable to store the modulo result
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Find the highest number in a set
+const set = [5, 10, 20, 15, 7];
+const max = Math.max(...set);
